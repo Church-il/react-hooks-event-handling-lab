5c09d55cff3ae54780c672293ea3533ceb1767ea
Michael Akoko <akokomichael37@gmail.com>
2024-08-31 17:17:56 +0300

    include jsx files

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
-// Code EyesOnMe Component Here
+// EyesOnMe.js
+import React from 'react';
+import './EyesOnMe.css';
+
+function EyesOnMe() {
+  const handleFocus = () => {
+    console.log('Good!');
+  };
+
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <div id="eyes-on-me">
+      <button onFocus={handleFocus} onBlur={handleBlur}>
+        Eyes on me
+      </button>
+    </div>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,38 @@
-// Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const handleChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <div style={styles.container}>
+      <input
+        type="password"
+        onChange={handleChange}
+        style={styles.input}
+        placeholder="Enter password"
+      />
+    </div>
+  );
 }
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    backgroundColor: '#f0f0f0',
+  },
+  input: {
+    fontSize: '1.5em',
+    padding: '10px',
+    border: '2px solid #007bff',
+    borderRadius: '5px',
+    outline: 'none',
+    width: '300px',
+  },
+};
 
 export default Keypad;
